d00cc39d1d28327c3ec3489c5e1e7b5b553108de
Marc from GitKraken <marcmauri92@gmail.com>
2020-03-11 00:11:35 +0100

    Resource Types
    Relation of companyZoneID with its kind of public transport

---
+++ app/src/main/java/es/marcmauri/meepmaptest/model/common/ResourceTypes.java
@@ -0,0 +1,12 @@
+package es.marcmauri.meepmaptest.model.common;
+
+public final class ResourceTypes {
+
+    public static final int BIKE     =  412;
+    public static final int BUS_ONE  =  402;
+    public static final int BUS_TWO  =  378;
+    public static final int BUS_THREE = 382;
+    public static final int ECOOLTRA =  473;
+    public static final int EMOV     =  467;
+    public static final int TAXI     =  545;
+}
